8b90198302034871e520319b9c8695e0aa7597be
xnot078 <xnot079@gmail.com>
2022-11-29 15:13:43 +0800

    [update]

--- ml_tools/__init__.py
+++ ml_tools/__init__.py
@@ -4,9 +4,13 @@ __all__ = [
     "version",
     "chain",
     "save_estimator",
-    "open_estimator"
+    "open_estimator",
+    "AnomalyExtractor",
+    "to_2d",
+    "cluster_plot"
 ]
 
 from .base import chain
 from .sklearn2json import save_estimator
 from .sklearn2json import open_estimator
+from .sk_tools import AnomalyExtractor, to_2d, cluster_plot
